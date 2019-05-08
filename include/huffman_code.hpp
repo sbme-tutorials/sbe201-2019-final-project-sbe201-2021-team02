68f7ee51f97090da7b0fcaf77be4900e262a9f56
MostafaAmr45 <mostafa.amr150@gmail.com>
2019-05-08 23:32:02 +0200

    final documentation

--- include/huffman_code.hpp
+++ include/huffman_code.hpp
@@ -16,7 +16,7 @@ struct huffman_node
 	huffman_node *right;
 
 	huffman_node()
-	{
+	{ //constructor for the initialization of the pointers
 		left = right = NULL;
 	}
 };
@@ -26,9 +26,9 @@ class huffman
 {
 
 protected:
-	ifstream in_file;
-	ofstream out_file;
-	node_ptr root;
+	ifstream in_file;	//declaration of an input file that I will read from
+	ofstream out_file;	//declaration of an output file that I will write into
+	node_ptr root;		//I will use it as the root of the huffman tree
 	char id;
 
 	class compare
@@ -40,22 +40,22 @@ protected:
 		}
 	};
 	priority_queue<node_ptr, vector<node_ptr>, compare> pq; //priority queue of frequency from high to low
-	map<char, int> charFreq;
-	map<char, string> charCode;
+	map<char, int> charFreq;	//map of characters and frequencies
+	map<char, string> charCode; //map of characters and huffman codes
 
 public:
 	string in_file_name, out_file_name;
-	void create_pq()
-	{
-		in_file.open(in_file_name, ios::in);
-		in_file.get(id);
-		while (!in_file.eof())
+	void create_pq()							
+	{ //make a priority queue using the frequencies of the characters in the file
+		in_file.open(in_file_name, ios::in);	//open the text file I want to compress
+		in_file.get(id);						//get the first character
+		while (!in_file.eof())					//iterate till the end of the file
 		{
-			charFreq[id]++;
+			charFreq[id]++;						// compute the frequencies of the characters and store it in the map
 			in_file.get(id);
 		}
 		in_file.close();
-		for (auto &[k, v] : charFreq)
+		for (auto &[k, v] : charFreq)			//the making of the priority queue using the frequency map
 		{
 			node_ptr temp = new huffman_node;
 			temp->id = k;
@@ -83,10 +83,10 @@ public:
 	}
 
 	void traverse(node_ptr node, string code = "")
-	{
+	{ //traverse over the tree to compute the huffman codes of the characters
 		if (node->left == NULL && node->right == NULL)
 		{
-			charCode[node->id] = code;
+			charCode[node->id] = code;	//after reaching a leaf store the character in a map with its corresponding code
 		}
 		else
 		{
@@ -96,23 +96,22 @@ public:
 	}
 
 	void calculate_huffman_codes()
-	{
+	{ //passing the root of the huffman tree to the traverse function to calculate the codes
 		traverse(root, "");
 	}
 
 	string encode()
-	{
+	{ //encode the text file into one string of huffman code
 		string encode;
 		create_pq();
-		typedef huffman_node *node_ptr;
 		create_huffman_tree(pq);
-		calculate_huffman_codes();
+		calculate_huffman_codes();	//getting the codes of the characters
 		in_file.open(in_file_name, ios::in);
 		in_file.get(id);
-		while (!in_file.eof())
+		while (!in_file.eof())	//iterate over the text file and convert every character into its corresponding code
 		{
-			encode += charCode[id];
-			in_file.get(id);
+			encode += charCode[id]; //insert the code of the character into the encode string
+			in_file.get(id);	//get the next character
 		}
 		in_file.close();
 
@@ -120,149 +119,42 @@ public:
 	}
 
 	string compress(const string code)
-	{
+	{	//compress the encode string which I have got from the previous function using a bitset to reduce its size
 		string compressed;
-		string str;
+		string str;				
 		str.clear();
-		for (auto c : code)
+		for (auto c : code)	//iterate over the encode string
 		{
 			str.push_back(c);
-			if (str.size() == 8)
-			{
-				char chr = bitset<8>(str).to_ulong();
+			if (str.size() == 8)	// takes 8 bits at a time and insert them into the bitset to compress them	
+			{ //convert binary to decimal and then convert that decimal to a character using the ASCII table
+				char chr = bitset<8>(str).to_ulong();	
 				compressed.push_back(chr);
-				str.clear();
-			}
+				str.clear();		//clear the string to push another 8 bits into it 
+			}						//repeat the process till the end of the encode string
 		}
-		return compressed;
-	}
+		return compressed;			//return the compressed string which I will print into the encoded file
+	}								//and that will be the compressed output file from the compressing process
 
 	void printTotxt(const string compressed)
-	{
-		out_file.open("encoded.txt");
-		map<char, int>::iterator it = charFreq.begin();
+	{ //print the compressed string into a text file
+		out_file.open("encoded.txt");						//arbitrary name for the file I will print into
+		map<char, int>::iterator it = charFreq.begin();		//iterator for iteration over the frequency map
 		for (it; it != charFreq.end(); it++)
-		{
+		{ //print the frequency map into the output file for using it in the decompressing process
 			out_file << it->first;
-			out_file << '%';
+			out_file << '%';								//seperator between the character and its frequency
 			out_file << it->second;
-			out_file << '^';
+			out_file << '^';								//seperator between the frequenct and the next frequency
 		}
 
-		out_file << '$';
+		out_file << '$';									//seperator between the map and the compressed encode string
 
 		for (int i = 0; i < compressed.size(); i++)
-		{
+		{ //print the compressed encode string into the output file
 			out_file << compressed[i];
 		}
 		out_file.close();
 	}
-
-	map<char, int> getMap()
-	{
-		map<char, int> freqMAp;
-		string v;
-		char k;
-		in_file.open(in_file_name, ios::in);
-
-		while (id != '$')
-		{
-			in_file.get(id);
-			if (id == '$')
-				break;
-
-			if (id == '%')
-				continue;
-			else
-				k = id;
-
-			in_file.get(id);
-			in_file.get(id);
-			while (id >= '0' && id <= '9')
-			{
-				if (id == '%' || id == '^')
-					break;
-
-				v += id;
-				in_file.get(id);
-			}
-			int freq = 0;
-			if (v.size() > 0)
-			{
-				freq = stoi(v);
-				freqMAp.insert(pair<char, int>(k, freq));
-			}
-			v.clear();
-		}
-		in_file.close();
-
-		return freqMAp;
-	}
-
-	node_ptr recalculate_huffman_codes()
-	{
-		priority_queue<node_ptr, vector<node_ptr>, compare> q;
-		map<char, int> freqMap = getMap();
-		for (auto &[k, v] : freqMap)
-		{
-			node_ptr temp = new huffman_node;
-			temp->id = k;
-			temp->freq = v;
-			q.push(temp);
-		}
-		return create_huffman_tree(q);
-	}
-
-	string decode()
-	{
-		string str;
-		str.clear();
-		in_file.open(in_file_name, ios::in);
-		in_file.get(id);
-		while (id != '$')
-		{
-			in_file.get(id);
-		}
-		in_file.get(id);
-
-		while (!in_file.eof())
-		{
-			in_file.get(id);
-			bitset<8> chr = id;
-			str += chr.to_string();
-		}
-		return str;
-	}
-
-	void decompress(node_ptr node, string code)
-	{
-		out_file.open("decoded.txt");
-
-		int i = 0;
-		node_ptr current = node;
-		while (i < code.size())
-		{
-			if (current->left == NULL && current->right == NULL)
-			{
-				out_file << current->id;
-				current = node;
-			}
-			if (code[i] == '1')
-				current = current->right;
-			else if (code[i] == '0')
-				current = current->left;
-
-			++i;
-		}
-		out_file.close();
-	}
-
-	void backToOriginal()
-	{
-		node_ptr temp;
-		temp = recalculate_huffman_codes();
-		decompress(temp, decode());
-	}
 };
-
 #endif
--- include/huffman_decode.hpp
+++ include/huffman_decode.hpp
@@ -16,7 +16,7 @@ struct huffman_node
 	huffman_node *right;
 
 	huffman_node()
-	{
+	{ //constructor for the initialization of the pointers
 		left = right = NULL;
 	}
 };
@@ -26,9 +26,9 @@ class huffman
 {
 
 protected:
-	ifstream in_file;
-	ofstream out_file;
-	node_ptr root;
+	ifstream in_file;	//declaration of an input file that I will read from
+	ofstream out_file;	//declaration of an output file that I will write into
+	node_ptr root;		//I will use it as the root of the huffman tree
 	char id;
 
 	class compare
@@ -40,30 +40,11 @@ protected:
 		}
 	};
 	priority_queue<node_ptr, vector<node_ptr>, compare> pq; //priority queue of frequency from high to low
-	map<char, int> charFreq;
-	map<char, string> charCode;
+	map<char, int> charFreq;	//map of characters and frequencies
+	map<char, string> charCode; //map of characters and huffman codes
 
 public:
 	string in_file_name, out_file_name;
-	void create_pq()
-	{
-		in_file.open(in_file_name, ios::in);
-		in_file.get(id);
-		while (!in_file.eof())
-		{
-			charFreq[id]++;
-			in_file.get(id);
-		}
-		in_file.close();
-		for (auto &[k, v] : charFreq)
-		{
-			node_ptr temp = new huffman_node;
-			temp->id = k;
-			temp->freq = v;
-			pq.push(temp);
-		}
-	}
-
 	node_ptr create_huffman_tree(priority_queue<node_ptr, vector<node_ptr>, compare> &q)
 	{
 		priority_queue<node_ptr, vector<node_ptr>, compare> temp(q);
@@ -82,129 +63,54 @@ public:
 		return temp.top();
 	}
 
-	void traverse(node_ptr node, string code = "")
-	{
-		if (node->left == NULL && node->right == NULL)
-		{
-			charCode[node->id] = code;
-		}
-		else
-		{
-			traverse(node->left, code + '0');
-			traverse(node->right, code + '1');
-		}
-	}
-
-	void calculate_huffman_codes()
-	{
-		traverse(root, "");
-	}
-
-	string encode()
-	{
-		string encode;
-		create_pq();
-		typedef huffman_node *node_ptr;
-		create_huffman_tree(pq);
-		calculate_huffman_codes();
-		in_file.open(in_file_name, ios::in);
-		in_file.get(id);
-		while (!in_file.eof())
-		{
-			encode += charCode[id];
-			in_file.get(id);
-		}
-		in_file.close();
-
-		return encode;
-	}
-
-	string compress(const string code)
-	{
-		string compressed;
-		string str;
-		str.clear();
-		for (auto c : code)
-		{
-			str.push_back(c);
-			if (str.size() == 8)
-			{
-				char chr = bitset<8>(str).to_ulong();
-				compressed.push_back(chr);
-				str.clear();
-			}
-		}
-		return compressed;
-	}
-
-	void printTotxt(const string compressed)
-	{
-		out_file.open("encoded.txt");
-		map<char, int>::iterator it = charFreq.begin();
-		for (it; it != charFreq.end(); it++)
-		{
-			out_file << it->first;
-			out_file << '%';
-			out_file << it->second;
-			out_file << '^';
-		}
-
-		out_file << '$';
-
-		for (int i = 0; i < compressed.size(); i++)
-		{
-			out_file << compressed[i];
-		}
-		out_file.close();
-	}
-
 	map<char, int> getMap()
-	{
+	{ //consruct the frequency map again from the compressed file to use it in the decomprssion process
 		map<char, int> freqMAp;
 		string v;
 		char k;
 		in_file.open(in_file_name, ios::in);
 
-		while (id != '$')
+		while (id != '$')		//terminate after reading the seperator between the map and the encode string
 		{
 			in_file.get(id);
 			if (id == '$')
-				break;
+				break;			//just to make sure I will really terminate when I see this sign :)
 
-			if (id == '%')
+			if (id == '%')		//continue if you see this seperator
 				continue;
 			else
-				k = id;
+				k = id;			// if you didn't see this '%' store the character in k
 
 			in_file.get(id);
 			in_file.get(id);
-			while (id >= '0' && id <= '9')
-			{
+			while (id >= '0' && id <= '9')	
+			{ //make sure that the next characters are only the frequency of the previous character
 				if (id == '%' || id == '^')
-					break;
+					break;					//To make sure I will print only the frequency
 
-				v += id;
+				v += id;					//store the frequency of the charater k in v
 				in_file.get(id);
 			}
 			int freq = 0;
 			if (v.size() > 0)
 			{
-				freq = stoi(v);
-				freqMAp.insert(pair<char, int>(k, freq));
+				freq = stoi(v);							  // casting the frequency from string to integers
+				freqMAp.insert(pair<char, int>(k, freq)); //insert the charater with its corresponding frequency in the map
 			}
-			v.clear();
+			v.clear();			//clear the frequency from the v string to make it ready to store the next frequency			
 		}
 		in_file.close();
 
 		return freqMAp;
 	}
 
-	node_ptr recalculate_huffman_codes()
-	{
+	node_ptr recreate_huffman_tree()
+	{ /*use the map I have got from the previous function to recreate the huffman tree 
+	  using the same logic used in the compression process*/
 		priority_queue<node_ptr, vector<node_ptr>, compare> q;
 		map<char, int> freqMap = getMap();
 		for (auto &[k, v] : freqMap)
-		{
+		{ 
 			node_ptr temp = new huffman_node;
 			temp->id = k;
 			temp->freq = v;
@@ -214,28 +120,29 @@ public:
 	}
 
 	string decode()
-	{
+	{ //read the compressed encode file and convert it into the original encoded one "the huffman code of the original file"
 		string str;
 		str.clear();
 		in_file.open(in_file_name, ios::in);
 		in_file.get(id);
 		while (id != '$')
-		{
+		{ // iterate to avoid reading the map
 			in_file.get(id);
 		}
-		in_file.get(id);
+		in_file.get(id); 			//read the first character of the compressed code
 
 		while (!in_file.eof())
 		{
 			in_file.get(id);
 			bitset<8> chr = id;
-			str += chr.to_string();
+			str += chr.to_string();	//convert the every character into its original binary code
 		}
-		return str;
+		return str;					//return the original huffman code
 	}
 
 	void decompress(node_ptr node, string code)
-	{
+	{ /*iterate over the code I have got from the previous function 
+	  and convert every code into its corresponding character to reach finally to the original text file*/
 		out_file.open("decoded.txt");
 
 		int i = 0;
@@ -243,11 +150,11 @@ public:
 		while (i < code.size())
 		{
 			if (current->left == NULL && current->right == NULL)
-			{
+			{ //print the character if you reach to a leaf and reset the current pointer to the root
 				out_file << current->id;
 				current = node;
 			}
-			if (code[i] == '1')
+			if (code[i] == '1') //traverse over the huffman tree till reaching a leaf
 				current = current->right;
 			else if (code[i] == '0')
 				current = current->left;
@@ -258,9 +165,9 @@ public:
 	}
 
 	void backToOriginal()
-	{
+	{ //recreate the huffman tree and decompressing the compressed file using the previous functions
 		node_ptr temp;
-		temp = recalculate_huffman_codes();
+		temp = recreate_huffman_tree();
 		decompress(temp, decode());
 	}
 };
