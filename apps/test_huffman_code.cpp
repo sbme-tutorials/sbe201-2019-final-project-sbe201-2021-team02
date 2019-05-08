44bf9828bac1fef105667a489da45e11c9b33574
MostafaAmr45 <mostafa.amr150@gmail.com>
2019-05-08 20:36:40 +0200

    CMake

---
+++ apps/test_huffman_code.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "huffman_code.hpp"
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		cout << "Please insert the file you want to compress" << endl;
+		exit(1);
+	}
+	huffman h;
+	h.in_file_name = argv[1];
+	string code = h.encode();
+	h.printTotxt(h.compress(code));
+
+	return 0;
+}
